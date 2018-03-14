3be5e925699b2f4d4c53c09e3854b1ba94f7b4aa
khoa developer <khoandph05225@fpt.edu.vn>
2018-03-14 15:45:36 +0700

    koko

--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Student {
     public static void main(String[] args) {
         System.out.println("hello");
+        Scanner sc = new Scanner(System.in );
     }
 }
